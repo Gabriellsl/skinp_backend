9c6efc80644b53e10b6fc71e666c94c520599ed9
Gabriel Lenin <gabriellsl2009@hotmail.com>
2020-05-18 23:47:38 +0100

    add of validations and image response on upload route

--- server.py
+++ server.py
@@ -1,27 +1,60 @@
-from flask import Flask, request
+import os
+from flask import Flask, flash, request, url_for, jsonify, send_file
 from PIL import Image
-
+from pathlib import Path
+from werkzeug.utils import secure_filename
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import datetime
+from threading import Thread
+import time
+
+basepath = os.path.dirname(__file__)
+path = os.path.abspath(os.path.join(basepath, ".", "upload_folder"))
 
 # used to avoid sending unwanted files
-ALLOWED_EXTENSIONS = {'jpg','png'}
+ALLOWED_EXTENSIONS = {'jpg', 'png'}
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return('test')
+def clean_folder(path):
+    time.sleep(5)
+    os.remove(path)
+    print('the image ',path,' as deleted!')
 
-@app.route('/upload/')
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload/', methods=['POST'])
 def uploadFile():
+    dateTime = str(datetime.now())
+    filename = "submited_img", dateTime, '.jpg'
+    filename = str(filename).replace(" ", "")
+    if 'file' not in request.files:
+        content = {
+            'Not file found': 'Please send file data with key=file'}
+        return jsonify(content), 400
+
     file = request.files['file']
-    print(type(file));
-    img = Image.open(file)
-    img = np.asarray(img)
-    plt.imshow(img)
-    plt.show()
-    return 'Successful'
+
+    if allowed_file(file.filename) == False:
+        content = {
+            'Extension not allowed': 'You must send file in jpg or png format'}
+        return jsonify(content), 400
+
+    # img = Image.open(file)
+    # img = np.asarray(img)
+    # plt.imshow(img)
+    # plt.show()
+    file_path = os.path.join(
+        path, secure_filename(filename))
+    file.save(file_path)
+
+    thread = Thread(target=clean_folder, kwargs={'path': file_path})
+    thread.start()
+
+    return send_file(file_path, mimetype='image/gif')
 
 if __name__ == '__main__':
     app.run(debug=True)
